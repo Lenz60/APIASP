51592fea09da5c3c2fbe05814507b96e074798d8
Lenz60 <raflyandrian@gmail.com>
2024-09-06 23:41:21 +0700

    Remove unecessary files

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ebd4f1bbf33cc97a020ba571da874fe910bd59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55962d3bbbc1da4aad502f7bcc44856b89cbd4d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
